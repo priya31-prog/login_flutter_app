68581138b8d2b7d5b3466da5f30d57ff8395f1f5
priya31-prog <karthika200103@gmail.com>
2025-03-21 07:45:16 +0530

    custom scaffold with linear gradient

---
+++ login_application/lib/common_files/custom_scaffold.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class CustomScaffold extends StatelessWidget {
+  final AppBar? appBar;
+  final Widget body;
+  final FloatingActionButton? floatingActionButton;
+  final Drawer? drawer;
+
+  const CustomScaffold({
+    required this.body,
+    this.appBar,
+    this.floatingActionButton,
+    this.drawer,
+  });
+
+  final LinearGradient bodyGradient = const LinearGradient(
+    colors: [Colors.greenAccent, Colors.white12],
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: bodyGradient,
+        ),
+        child: body,
+      ),
+      floatingActionButton: floatingActionButton,
+      drawer: drawer,
+    );
+  }
+}
