2c773e081cec54230ec0c39dfa943a3824f33177
priya31-prog <karthika200103@gmail.com>
2025-03-21 07:06:47 +0530

    basic detail creation

---
+++ login_application/lib/detail_page/about.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatefulWidget {
+  const AboutPage({super.key});
+
+  // final String title;
+
+  @override
+  State<AboutPage> createState() => _AboutPage();
+}
+
+class _AboutPage extends State<AboutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
