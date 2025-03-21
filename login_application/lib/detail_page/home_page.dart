f805cdad6a92844181cc809cdf0e62e3f3856b03
priya31-prog <karthika200103@gmail.com>
2025-03-21 08:17:54 +0530

    50% completed home page

--- login_application/lib/detail_page/home_page.dart
+++ login_application/lib/detail_page/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:login_application/common_files/app_constants.dart';
+import 'package:login_application/common_files/custom_scaffold.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -12,9 +14,66 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePage extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-      title: Text(widget.title),
-    ));
+    return CustomScaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: Container(
+              width: 70,
+              height: 70,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.greenAccent.withOpacity(0.3),
+                      offset: const Offset(0, 0.3),
+                      spreadRadius: 2,
+                      blurRadius: 7,
+                    ),
+                  ]),
+            ),
+          ),
+          Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                AppConstants.welcomeTitle,
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                AppConstants.discover,
+                style: TextStyle(
+                  fontSize: 18,
+                  // fontWeight: FontWeight.w700,
+                ),
+              ),
+              const SizedBox(
+                height: 70,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.secondary,
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    AppConstants.homebtnText,
+                    style: TextStyle(color: Colors.white),
+                  ))
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
