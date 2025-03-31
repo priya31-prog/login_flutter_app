e0713a49b105e70e0b7fdc507941686e45ac1f59
priya31-prog <karthika200103@gmail.com>
2025-03-31 10:06:53 +0530

    delete app

--- login_application/lib/common_files/app_constants.dart
+++
@@ -1,6 +0,0 @@
-class AppConstants {
-  static const String appTitle = 'Login App';
-  static const String welcomeTitle = 'Welcome to Login App';
-  static const String discover = 'Discover the possibillities with us!';
-  static const String homebtnText = 'Get Started';
-}
