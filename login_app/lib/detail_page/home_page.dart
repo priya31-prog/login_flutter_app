e786e0e79c76a9fb50c0d2dec31b0610e7b8a3cd
priya31-prog <karthika200103@gmail.com>
2025-04-03 09:05:59 +0530

    newly added files

---
+++ login_app/lib/detail_page/home_page.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:login_app/common_files/app_constants.dart';
+// import 'package:login_app_sample/common_files/app_constants.dart';
+
+import '../common_files/custom_scaffold.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: Container(
+              width: 70,
+              height: 70,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.greenAccent.withOpacity(0.3),
+                    offset: const Offset(0, 0.3),
+                    spreadRadius: 2,
+                    blurRadius: 7,
+                  ),
+                ],
+              ),
+              child: Center(
+                child: SizedBox(
+                  height: 40,
+                  width: 40,
+                  child: Image.asset(
+                    'assets/images/hand_phone.jpg',
+                    errorBuilder: (
+                      BuildContext context,
+                      Object error,
+                      StackTrace? stackTrace,
+                    ) {
+                      return Text('Image failed to load');
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                AppConstants.welcomeTitle,
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.w700),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                AppConstants.discover,
+                style: TextStyle(
+                  fontSize: 18,
+                  // fontWeight: FontWeight.w700,
+                ),
+              ),
+              const SizedBox(height: 70),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.secondary,
+                ),
+                onPressed: () {},
+                child: const Text(
+                  AppConstants.homebtnText,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
