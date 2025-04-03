0a458c0b03bdf11b047a7600cfd07666121669c8
priya31-prog <karthika200103@gmail.com>
2025-04-03 09:45:35 +0530

    un used files

--- login_app/lib/detail_page/sign_in_page.dart
+++ login_app/lib/detail_page/sign_in_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 import 'package:login_app/common_files/custom_scaffold.dart';
 // import 'package:login_app_sample/common_files/custom_scaffold.dart';
 
