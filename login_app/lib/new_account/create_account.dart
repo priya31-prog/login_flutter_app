cd464e6d9ab0594c4d8d8c2ad53a13e871db79e0
priya31-prog <karthika200103@gmail.com>
2025-04-07 08:21:06 +0530

    create account dummy page

---
+++ login_app/lib/new_account/create_account.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateAccount extends StatefulWidget {
+  const CreateAccount({super.key});
+
+  @override
+  State<CreateAccount> createState() => _CreateAccount();
+}
+
+class _CreateAccount extends State<CreateAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
