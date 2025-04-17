f0c2efb613dc05130584164941bd3d671a771a4e
priya31-prog <karthika200103@gmail.com>
2025-04-17 10:21:56 +0530

    shared preference implementation

--- login_app/lib/new_account/create_account.dart
+++ login_app/lib/new_account/create_account.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:login_app/common_files/custom_scaffold.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CreateAccount extends StatefulWidget {
   const CreateAccount({super.key});
@@ -8,8 +10,131 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccount extends State<CreateAccount> {
+  final _userNameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  String userName = '';
+
+  void clearText() {
+    _userNameController.clear();
+    _emailController.clear();
+    _passwordController.clear();
+  }
+
+  Future<void> saveData(
+    final String username,
+    final String email,
+    final String password,
+  ) async {
+    final shared = await SharedPreferences.getInstance();
+    await shared.setString('userName', username);
+  }
+
+  Future<void> getData() async {
+    final shared = await SharedPreferences.getInstance();
+    String? username = shared.getString('username');
+    print('user name is $username');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return CustomScaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text('Create an account'),
+        centerTitle: true,
+        foregroundColor: Colors.white,
+        backgroundColor: Color(0x44000000),
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).size.height * 0.14,
+          left: 8,
+          right: 8,
+        ),
+        child: Form(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text('Username', style: TextStyle(fontWeight: FontWeight.bold)),
+              SizedBox(height: 8),
+              TextField(
+                controller: _userNameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  hintText: 'enter username',
+                  hintStyle: TextStyle(fontSize: 14),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text('Email', style: TextStyle(fontWeight: FontWeight.bold)),
+              SizedBox(height: 8),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  hintText: 'email',
+                  hintStyle: TextStyle(fontSize: 14),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text('Password', style: TextStyle(fontWeight: FontWeight.bold)),
+              SizedBox(height: 8),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  hintText: 'password',
+                  hintStyle: TextStyle(fontSize: 14),
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        userName = _userNameController.text;
+                        saveData(
+                          _userNameController.text,
+                          _emailController.text,
+                          _passwordController.text,
+                        );
+                        clearText();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        foregroundColor: Colors.white,
+                      ),
+                      child: Text('Create'),
+                    ),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  getData();
+                  clearText();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  foregroundColor: Colors.white,
+                ),
+                child: Text('getData'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
